5eeb422630e85986febfaf689dbab4465eeae44e
Andrea Canciani <ranma42@gmail.com>
2015-05-28 17:19:15 +0200

    working

--- coda.fs
+++ coda.fs
@@ -36,7 +36,27 @@ module Runtime =
 
   let mutable private trace = false
 
-  type private Context() =
+  let private genSym (boundvars:Set<_>) prefix =
+    let rec genSymAux suffix =
+      let name = sprintf "%s%d" prefix suffix
+      if boundvars.Contains(name) then
+        genSymAux (suffix+1)
+      else
+        name
+    if boundvars.Contains(prefix) then
+      genSymAux 0
+    else
+      prefix
+
+  let rec private exprVars = function
+    | ShapeCombination(comb,args) -> List.map exprVars args |> Set.unionMany
+    | ShapeLambda(v,body) -> exprVars body |> Set.add v.Name
+    | ShapeVar var -> Set.singleton var.Name
+
+  type private Context() = 
+    let enumType = typeof<IEnumerator<bool>>
+    let castPred g : Expr<seq<bool>> = Expr.Cast g
+
     member this.Tell(Fact(name, args)) =
       if trace then
         printfn "Assert %A %A" name args
@@ -48,37 +68,37 @@ module Runtime =
       for _ in YP.retract(Functor.make(name, args)) do
         ()
 
-    member this.Enumerate(goal:seq<bool>[], vars:Dictionary<string,obj>, vals:Dictionary<string,obj>) =
+    member this.QuoteEnumerate(goal:Expr[], body:Expr) = 
       if trace then
-        printfn "Enumerate %A %A" goal vars
-      let rec iterSols = function
-      | h::t -> seq { for _ in h do for () in iterSols t do yield () }
-      | [] ->
-        if vars = null then
-          seq { yield () }
-        elif trace then
-          seq {
-            printfn "Solution:"
-            for entry in vars do
-              vals.[entry.Key] <- (entry.Value :?> Variable).getValue()
-              printfn " %A -> %A" (entry.Key) (entry.Value)
-            yield ()
-          }
-        else
-          seq {
-            for entry in vars do
-              vals.[entry.Key] <- (entry.Value :?> Variable).getValue()
-            yield ()
-          }
-      goal |> Array.toList |> iterSols
+        printfn "QuoteEnumerate\n%A\n%s" [for g in goal -> g.Decompile()] (body.Decompile())
+      
+      body  
+      |> Array.foldBack (fun g s -> <@@ for (x:bool) in %(castPred g) do %%s @@>) goal
 
-    member this.Solve(goal:seq<bool>[], solution:Dictionary<string,obj>) =
+    member this.QuoteSolve(goal:Expr[], thenExpr:Expr, elseExpr:Expr) =
       if trace then
-        printfn "Solve %A %A" goal solution
-      let vars = if solution = null then null else new Dictionary<_,_>(solution)
-      this.Enumerate(goal, vars, solution)
-      |> Seq.tryPick Some
-      |> Option.isSome
+        printfn "QuoteSolve\n%A\n%s\n%s" [for g in goal -> g.Decompile()] (thenExpr.Decompile()) (elseExpr.Decompile())
+      let mutable vars = Set.union (exprVars thenExpr) (exprVars elseExpr)
+      let enumVars = Array.zeroCreate goal.Length
+      for i = 0 to goal.Length-1 do
+        let name = genSym vars "enum"
+        vars <- Set.add name vars
+        enumVars.[i] <- Var(name, enumType)
+
+      // use e0 = (%goal.[0]).GetEnumerator()
+      // use e1 = (%goal.[1]).GetEnumerator()
+      // ...
+      // if e0.MoveNext && e1.MoveNext && .. then %thenExpr else %elseExpr
+
+      let enumExprs:Expr<IEnumerator<bool>>[] = Array.map (Expr.Var >> Expr.Cast) enumVars
+      let enums = goal |> Array.map (fun g -> <@ (%(castPred g)).GetEnumerator() @>)
+      let disposes = enumExprs |> Array.map (fun e -> <@ (%e).Dispose() @>)
+      let moves = enumExprs |> Array.map (fun e -> <@ (%e).MoveNext() @>)
+      let guard = moves |> Array.reduceBack (fun a b -> <@ (%a) && (%b) @>)
+
+      Expr.IfThenElse(guard, thenExpr, elseExpr)
+      |> Array.foldBack (fun e s -> Expr.TryFinally(s, e)) disposes
+      |> Array.foldBack2 (fun v e s -> Expr.Let(v, e, s)) enumVars enums
 
   // Global context, only accessible by the runtime
   let private context = new Context()
@@ -111,18 +131,9 @@ module Runtime =
 
   let private isCodaFunction = function MethodWithReflectedDefinition(quot) -> true | _ -> false
 
-  let private genSym (boundvars:Set<_>) prefix =
-    let rec genSymAux suffix =
-      let name = sprintf "%s%d" prefix suffix
-      if boundvars.Contains(name) then
-        genSymAux (suffix+1)
-      else
-        name
-    genSymAux 0
-
-  let private solutionType = typeof<Dictionary<string,obj>>
+  let private varType = typeof<YieldProlog.Variable>
   let private unitType = <@ () @>.Type
-  let private currentProperty = typeof<System.Collections.Generic.IEnumerator<unit>>.GetProperty("Current")
+  //let private currentProperty = typeof<System.Collections.Generic.IEnumerator<unit>>.GetProperty("Current")
 
   let private getTypedPred (mi:System.Reflection.MethodInfo) =
     if mi.GetCustomAttributes(typeof<TypedPredAttribute>, false).Length <> 0 then
@@ -138,47 +149,35 @@ module Runtime =
     else
       mi
 
-  let private solVarToExpr vars solVar : Expr<Dictionary<string,obj>> =
-    if Set.isEmpty vars then
-      <@ null @>
-    else
-      Expr.Var(solVar) |> Expr.Cast
+  let private makeSolBinding vars body =
+    Map.fold (fun s name v -> Expr.Let(v, <@@ new YieldProlog.Variable() @@>, s)) body vars
 
-  let private makeSolBinding vars solVar body =
-    let solExpr = solVarToExpr vars solVar
-    let rec aux body = function
-      | [] -> Expr.Let(solVar, <@ new Dictionary<string,obj>() @>, body)
-      | hd::tl -> aux (Expr.Sequential(<@@ (%solExpr).[hd] <- new YieldProlog.Variable() @@>, body)) tl
-    if Set.isEmpty vars then
-      body
-    else
-      aux body (Set.toList vars)
-
-  let rec private compileCoda (boundVars:Set<_>) (dboundVars:Map<_,_>) (vaVars:Set<_>) vaExpr expr =
-    let compile = compileCoda boundVars dboundVars vaVars vaExpr
+  let rec private compileCoda (boundVars:Set<_>) (dboundVars:Map<_,_>) (vaVars:Map<_,_>) expr =
+    let compile = compileCoda boundVars dboundVars vaVars
     let boxQuote q = Expr.Coerce(compile q, typeof<obj>)
 
     let compileGoal goal =
-      let solVar = Var(genSym boundVars "solution", solutionType)
-      let solExpr:Expr<Dictionary<string,obj>> = Expr.Var(solVar) |> Expr.Cast
-      let ctxArgs =
-        let tryCtxArgName  = function SpecificCall<@ (?) @>(None, _, [PropertyGet(None, ctx, []); Value(name, t)]) -> Some (name :?> string) | _ -> None
-        let callCtxArgs = function Call(None, mi, args) -> List.choose tryCtxArgName args | p -> []
-        let goalCtxArgs = function NewTuple(l) -> List.collect callCtxArgs l | g -> callCtxArgs g
-        goal
-        |> goalCtxArgs
-        |> Set.ofList
+      let tryCtxArgName  = function SpecificCall<@ (?) @>(None, _, [PropertyGet(None, ctx, []); Value(name, t)]) -> Some (name :?> string) | _ -> None
+      let callCtxArgs = function Call(None, mi, args) -> List.choose tryCtxArgName args | p -> []
+      let goalCtxArgs = function NewTuple(l) -> List.collect callCtxArgs l | g -> callCtxArgs g
+      let distinctNames = goal |> goalCtxArgs |> Set.ofList |> Set.toArray
+      let syms = Array.zeroCreate distinctNames.Length
+      let mutable boundNames = boundVars
+      for i = 0 to distinctNames.Length-1 do
+        let name = genSym boundNames distinctNames.[i]
+        boundNames <- boundNames.Add(name)
+        syms.[i] <- Var(name, typeof<YieldProlog.Variable>)
+      let ctxArgs = Array.zip distinctNames syms |> Map.ofArray
       let goalExpr =
         let compileArg = function
           | SpecificCall<@ (?) @>(None, _, [PropertyGet(None, ctx, []); Value(name, t)]) ->
-            let name = name :?> string
-            <@ (%solExpr).[name] @>.Raw
+            Expr.Var(ctxArgs.[name :?> string])
           | Coerce(v, t) as expr -> Expr.Coerce(compile expr, t)
           | expr -> compile expr
         let compilePred = function Call(None, mi, args) -> Expr.Call(getTypedPred mi, List.map compileArg args) | p -> compile p
         let compileSubgoals = function NewTuple(l) -> List.map compilePred l | g -> [ compilePred g ]
-        Expr.NewArray(typeof<seq<bool>>, compileSubgoals goal)
-      ctxArgs,solVar,goalExpr
+        compileSubgoals goal |> List.toArray
+      ctxArgs,goalExpr
 
     match expr with
       // tell
@@ -191,20 +190,19 @@ module Runtime =
 
       // Behavioural variation
       | IfThenElse(SpecificCall<@ (!-) @>(None, _, [goal]), thenBody, elseBody) ->
-        let vaVars,solVar,goal = compileGoal goal
-        let boundVars = boundVars.Add(solVar.Name)
-        let vaExpr = solVarToExpr vaVars solVar
-        Expr.IfThenElse(<@ context.Solve(%%goal, %vaExpr) @>,
-                        compileCoda boundVars dboundVars vaVars vaExpr thenBody,
-                        compile elseBody)
-        |> makeSolBinding vaVars solVar
+        let vaVars,goals = compileGoal goal
+        let boundVars = boundVars + Set(vaVars |> Map.toSeq |> Seq.map fst)
+        let thenExpr = compileCoda boundVars dboundVars vaVars thenBody
+        let elseExpr = compile elseBody
+        context.QuoteSolve(goals, thenExpr, elseExpr)
+        |> makeSolBinding vaVars
 
       | SpecificCall<@ (?) @>(None, _, [PropertyGet(None, ctx, []); Value(name, t)]) -> // consume variable
-        let name = name :?> string
-        if vaVars.Contains(name) then
-          Expr.Coerce(<@@ (%(vaExpr)).[name] @@>, expr.Type)
-        else
-          failwith "Cannot use undefined Context-Dependent variables"
+        match vaVars.TryFind(name :?> string) with
+        | Some v ->
+          let e:Expr<YieldProlog.Variable> = Expr.Var(v) |> Expr.Cast
+          Expr.Coerce(<@@ (%e).getValue() @@>, expr.Type)
+        | None -> failwith "Cannot use undefined Context-Dependent variables"
 
       | SpecificCall<@ (?) @>(_,_,_) -> // (?) misuse
         failwith "The ? syntax is only allowed on the ctx object and only for solved goals"
@@ -214,49 +212,46 @@ module Runtime =
 
 
       // for
-      | Let(s, SpecificCall<@ (!--) @> (None, _, [goal]), body) ->
+      | Let(s, SpecificCall<@ (!--) @> (None, _, [goal]),
+            Let(_, _,
+                TryFinally(WhileLoop(_,
+                                     Let(_, _, body)), _))) ->
         let boundVars = boundVars.Add(s.Name)
-        let vaVars,solVar,goal = compileGoal goal
-        let boundVars = boundVars.Add(solVar.Name)
-        let varVar = Var(genSym boundVars "variables", solutionType)
-        let boundVars = boundVars.Add(varVar.Name)
-        let valExpr = solVarToExpr vaVars solVar
-        let varExpr = solVarToExpr vaVars varVar
-        let expr = Expr.Let(s,
-                            <@ context.Enumerate(%%goal, %varExpr, %valExpr) @>,
-                            compileCoda boundVars dboundVars vaVars valExpr body)
-        if Set.isEmpty vaVars then
-          expr
-        else
-          Expr.Let(varVar, <@ new Dictionary<string,obj>(%valExpr) @>, expr)
-        |> makeSolBinding vaVars solVar
-        
+        let vaVars,goals = compileGoal goal
+        let boundVars = boundVars + Set(vaVars |> Map.toSeq |> Seq.map fst)
+        let body = compileCoda boundVars dboundVars vaVars body
+        context.QuoteEnumerate(goals, body)
+        |> makeSolBinding vaVars
+
+      | SpecificCall<@ (!--) @>(_,_,_) -> // (!-) misuse
+        failwith "The !-- syntax is only allowed for context-dependent iteration"
+
 
       // Dlet
       | Let(var, SpecificCall<@ (|-) @>(None, _, [v; goal]), body) ->
         let name = var.Name
-        let boundVars = boundVars.Add(name)
+
         let fallbackValue =
           match dboundVars.TryFind(name) with
             | None -> Expr.Coerce(<@@ raise <| InconsistentContext() @@>, v.Type)
             | Some lazyFallback -> Expr.Application(lazyFallback, <@ () @>)
-        let vaVars,solVar,goal = compileGoal goal
-        let boundVars = boundVars.Add(solVar.Name)
-        let vaExpr = solVarToExpr vaVars solVar
-        let value = Expr.IfThenElse(<@ context.Solve(%%goal, %vaExpr) @>,
-                        compileCoda boundVars dboundVars vaVars vaExpr v,
-                        fallbackValue)
+
+        let boundVars = boundVars.Add(name)
+        let vaVars,goals = compileGoal goal
+        let boundVars = boundVars + Set(vaVars |> Map.toSeq |> Seq.map fst)
+        let thenExpr = compileCoda boundVars dboundVars vaVars v
+        let value = context.QuoteSolve(goals, thenExpr, fallbackValue)
         let lazyValue = Expr.Lambda(Var("unused", unitType),
-                                    makeSolBinding vaVars solVar value)
+                                    makeSolBinding vaVars value)
         let lazyVar = Var(name, lazyValue.Type)
         let dboundVars = dboundVars.Add(name, Expr.Var(lazyVar))
         Expr.Let(lazyVar, lazyValue,
-                 compileCoda boundVars dboundVars vaVars vaExpr body)
+                 compileCoda boundVars dboundVars vaVars body)
 
       | Let(var, value, body) -> // normal let
          let dboundVars = dboundVars.Remove(var.Name)
          Expr.Let(var, compile value,
-                  compileCoda boundVars dboundVars vaVars vaExpr body)
+                  compileCoda boundVars dboundVars vaVars body)
 
       | Var(v) ->
         match dboundVars.TryFind(v.Name) with
@@ -290,15 +285,15 @@ module Runtime =
         if trace then
           printfn "Trampoline! %A(%A) [%A/%A]" mi args (args.Length) (types.Length)
           printfn "%A" mi
-          printfn "%s" (quot.Decompile())
+          printfn "%s" <| quot.Decompile()
 
         if types.Length <> args.Length then
           failwith "Mismatch between args and signature"
 
-        let translated = compileCoda (Set.empty) (Map.empty) (Set.empty) (Unchecked.defaultof<_>) quot
+        let translated = compileCoda (Set.empty) (Map.empty) (Map.empty) quot
         if trace then
           printfn "translated to"
-          printfn "%s" (translated.Decompile())
+          printfn "%s" <| translated.Decompile()
 
         let call =
           if args.Length = 0 then
@@ -328,7 +323,7 @@ module Runtime =
         | _ -> failwith "Entry point not found!"
 
     for attr in entryPoint.GetCustomAttributes(typeof<ContextAttribute>, false) do
-      initContext (Assembly.Load((attr :?> ContextAttribute).Name))
+      initContext <| Assembly.Load((attr :?> ContextAttribute).Name)
     // run the entry point
     callTramp entryPoint args |> ignore
 
@@ -338,5 +333,5 @@ module Runtime =
 
   let debug([<ParamArray>] args) =
     trace <- true
-    printfn "Running %A" (Assembly.GetEntryAssembly())
+    printfn "Running %A" <| Assembly.GetEntryAssembly()
     startEntryPoint [| |]
